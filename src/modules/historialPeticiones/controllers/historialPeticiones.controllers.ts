20fb27172868f5d6ac5fb2320aab4c26dacdff3b
Bastian1212 <bastianvillanueva@me.com>
2022-11-22 21:44:26 -0300

    falta setHistorial y nueva actualizacion

--- src/modules/historialPeticiones/controllers/historialPeticiones.controllers.ts
+++ src/modules/historialPeticiones/controllers/historialPeticiones.controllers.ts
@@ -29,6 +29,21 @@ class HistorialPeticionesController {
     }
 
     public setHistorial(request : Request, response : Response){
+        // const DELETE_QUERY = `SELECT id_imm FROM historialpeticiones WHERE id_imm = '${req.body.id}' AND tipo = ${req.body.tipo}`
+        // connection.query(DELETE_QUERY, (err, res) =>{
+        //     if(err) console.log(err)
+        //     else{
+        //         var idIndicadores = res.map(function(x) {
+        //             return x.id;
+        //          });
+        //          for(let i=0; i < idIndicadores.length ; i++){ 
+        //             const ADD_QUERY = `UPDATE historialpeticiones SET id_imm = '${req.body.D}' WHERE id_imm = "${req.body.id}" AND tipo = ${req.body.tipo};`
+        //             connection.query(ADD_QUERY, (err) =>{
+        //                 if(err) console.log(err)
+        //             })   
+        //         }
+        //     }
+        // })
 
     }
 }
